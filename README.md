c32c553c22219ce61d045c2477b1a78a5f1b913f
luckman212 <1992842+luckman212@users.noreply.github.com>
2025-09-21 12:30:12 -0400

    modified ContactsSearch.alfredworkflow, modified README.md, modified contacts_db_search.sh

--- README.md
+++ README.md
@@ -15,13 +15,23 @@ It also works around a limitation imposed by macOS 26 ("Tahoe") where Spotlight 
 - Hold the <kbd>⌘COMMAND</kbd> key while actioning a contact to reveal that contact's .abcdp file in Finder.
 - Hold the <kbd>⌥OPTION</kbd> key while actioning a contact to open the contact in the native Contacts.app.
 
+### Narrowing Scope
+
+You can limit the search results to a specific AddressBook in the Workflow Configuration area. Leave blank to include all addressbooks found in the default location (`~/Library/Application Support/AddressBook/Sources`)
+
+Additionally, you can choose specific GUIDs to exclude, by adding them to the **Exclusions** field in the config. The GUID is the string of HEX digits in the AddressBook path, e.g. `0414758E-7607-4C67-8EA1-CFDDCB4FDE5C`.
+
+These features can be combined so Alfred returns only the results you desire.
+
 ### Credits
 
 Thanks to [FireFingers21](https://www.alfredforum.com/topic/16269-contacts-search-only-with-keyword/page/2/#findComment-122641) for inspiration and the excellent SQL query that was adapted for use here.
 
 ### Requirements
 
-The workflow depends on [`jq`](http://jqlang.org/) for JSON processing. If needed, please install this using your preferred method (probably `brew install jq`). It also almost certainly requires Alfred to have Full Disk Access.
+The workflow depends on [jq](http://jqlang.org/) for JSON processing. If you're using macOS 15 or later, `jq` is already preinstalled. Otherwise, please install it using your preferred method (probably `brew install jq`).
+
+The workflow also requires Alfred to have Full Disk Access so it can access your AddressBook data.
 
 ### Further Discussion
 
--- contacts_db_search.sh
+++ contacts_db_search.sh
@@ -5,97 +5,127 @@
 
 # requires jq: brew install jq
 
-BASE_DIR="$HOME/Library/Application Support/AddressBook/Sources"
+_dbg() {
+	(( DEBUG == 1 )) || return 0
+	cat >&2 <<-EOF
+	🪵DEBUG: $1
+	EOF
+}
+
+_get_dbs() {
+	if [[ -z $EXCLUDED_GUIDS ]]; then
+		_dbg "no exclusions"
+		find "$BASE_DIR" -type f -name "$ADBK_DB" -maxdepth 2
+	else
+		_dbg "excluded GUIDs:${CR}${EXCLUDED_GUIDS}"
+		find "$BASE_DIR" -type f -name "$ADBK_DB" -maxdepth 2 |
+		grep --ignore-case --invert-match --fixed-strings --file <(echo "$EXCLUDED_GUIDS")
+	fi
+}
+
+_dbg "${0:t} script started"
+
 ADBK_DB='AddressBook-v22.abcddb'
+CR=$'\n'
+if [[ -z $BASE_DIR ]]; then
+	BASE_DIR="$HOME/Library/Application Support/AddressBook/Sources"
+	_dbg "using DEFAULT sources dir: $BASE_DIR"
+else
+	_dbg "using CUSTOM sources dir: $BASE_DIR"
+fi
 
-while read -r DB ; do sqlite3 -json "$DB" <<EOF
+_dbg "sort order: $SORT_BY"
 
-SELECT
-    '$DB' as DB,
-    r.ZTITLE,
-    r.ZFIRSTNAME,
-    r.ZMIDDLENAME,
-    r.ZLASTNAME,
-    r.ZNICKNAME,
-    r.ZORGANIZATION,
-    r.ZJOBTITLE,
-    r.ZUNIQUEID,
-    r.ZMODIFICATIONDATE,
-    i.ZSTRINGFORINDEXING
-FROM
-    ZABCDRECORD r
-LEFT JOIN
-    ZABCDCONTACTINDEX i ON i.ZCONTACT = r.Z_PK
-WHERE
-    r.ZCONTACTINDEX IS NOT NULL
-GROUP BY
-    r.ZUNIQUEID;
-EOF
-
-done < <(find "$BASE_DIR" -type f -name "$ADBK_DB" -depth 2) |
+while read -r DB ; do
+	(( c++ ))
+	SOURCE_DIR=${DB:A:h}
+	_dbg "processing source $c: $SOURCE_DIR"
+	sqlite3 -json "$DB" <<-EOF
+	SELECT
+		'$SOURCE_DIR' as DBPATH,
+		r.ZTITLE,
+		r.ZFIRSTNAME,
+		r.ZMIDDLENAME,
+		r.ZLASTNAME,
+		r.ZNICKNAME,
+		r.ZORGANIZATION,
+		r.ZJOBTITLE,
+		r.ZUNIQUEID,
+		r.ZMODIFICATIONDATE,
+		i.ZSTRINGFORINDEXING
+	FROM
+		ZABCDRECORD r
+	LEFT JOIN
+		ZABCDCONTACTINDEX i ON i.ZCONTACT = r.Z_PK
+	WHERE
+		r.ZCONTACTINDEX IS NOT NULL
+	GROUP BY
+		r.ZUNIQUEID;
+	EOF
+done < <(_get_dbs) |
 
 jq \
-    --null-input \
-    --compact-output \
-    --arg dbname "$ADBK_DB" \
-    --arg sortby "$SORT_BY" \
-    --argjson ct ${CACHE_SECS:-10} '
+	--null-input \
+	--compact-output \
+	--arg dbname "$ADBK_DB" \
+	--arg sortby "$SORT_BY" \
+	--argjson ct ${CACHE_SECS:-60} '
 
-    def join_nonempty(sep):
-        map(select(.)) | join(sep);
-    def join_nonempty:
-        join_nonempty(" ");
-    def trim_i:
-        sub("^\\s+";"") | sub("\\s+$";"");
+	def join_nonempty(sep):
+		map(select(.)) | join(sep);
+	def join_nonempty:
+		join_nonempty(" ");
+	def trim_i:
+		sub("^\\s+";"") | sub("\\s+$";"");
 
-    [inputs] | reduce .[] as $item ([]; . + $item) |
+	[inputs] | reduce .[] as $item ([]; . + $item) |
 
-    {
-        cache: { seconds: $ct, loosereload: true },
-        skipknowledge: true,
-        items: (
-            if length > 0 then
-                map(
-                    .dbpath = (.DB | sub($dbname; "")) |
-                    .name = ([ .ZFIRSTNAME, .ZMIDDLENAME, .ZLASTNAME ] | join_nonempty) |
-                    if .ZJOBTITLE then
-                        if (.name | length > 0) then
-                            .name += " (\(.ZJOBTITLE))"
-                        else
-                            .name = .ZJOBTITLE
-                        end
-                    end |
-                    if (.name | length > 0) then
-                        .title = ([ .name, .ZORGANIZATION ] | join_nonempty(" - ")) |
-                        .type = "person"
-                    else
-                        .title = .ZORGANIZATION |
-                        .type = "org"
-                    end |
-                    .filename = "\(.dbpath)/Metadata/\(.ZUNIQUEID).abcdp" |
-                    .match = .ZSTRINGFORINDEXING |
-                    {
-                        uid: .ZUNIQUEID,
-                        mod: .ZMODIFICATIONDATE,
-                        match: .match,
-                        title: .title,
-                        arg: .filename,
-                        icon: { path: "\(.type).png" },
-                        text: {
-                            copy: .title,
-                            largetype: .title
-                        },
-                        action: { text: .title },
-                        quicklookurl: .filename
-                    }
-                ) |
-                if $sortby == "date" then
-                    sort_by(-.mod)
-                else
-                    sort_by(.title // "<No name>" | trim_i | ascii_downcase)
-                end                
-            else
-                [{ title: "No contacts found", valid: false }]
-            end
-        )
-    }'
+	{
+		cache: { seconds: $ct, loosereload: true },
+		skipknowledge: true,
+		items: (
+			if length > 0 then
+				map(
+					.name = ([ .ZFIRSTNAME, .ZMIDDLENAME, .ZLASTNAME ] | join_nonempty) |
+					if .ZJOBTITLE then
+						if (.name | length > 0) then
+							.name += " (\(.ZJOBTITLE))"
+						else
+							.name = .ZJOBTITLE
+						end
+					end |
+					if (.name | length > 0) then
+						.title = ([ .name, .ZORGANIZATION ] | join_nonempty(" - ")) |
+						.type = "person"
+					else
+						.title = .ZORGANIZATION |
+						.type = "org"
+					end |
+					.filename = "\(.DBPATH)/Metadata/\(.ZUNIQUEID).abcdp" |
+					{
+						uid: .ZUNIQUEID,
+						mod: .ZMODIFICATIONDATE,
+						match: .ZSTRINGFORINDEXING,
+						title: .title,
+						arg: .filename,
+						icon: { path: "\(.type).png" },
+						text: {
+							copy: .title,
+							largetype: .title
+						},
+						action: { text: .title },
+						quicklookurl: .filename
+					}
+				) |
+				if $sortby == "date" then
+					sort_by(-.mod)
+				else
+					sort_by(.title // "<No name>" | trim_i | ascii_downcase)
+				end
+			else
+				[{ title: "No contacts found", valid: false }]
+			end
+		)
+	}'
+
+_dbg "${0:t} script finished"
